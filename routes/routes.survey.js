b5dabc9248829ae8ade4653f3e0c73efd85e3da5
NewDivide41194 <heinhtet25944@gmail.com>
2021-05-20 11:46:25 +0630

    update ans

--- routes/routes.survey.js
+++ routes/routes.survey.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const {surveyController}=require("../controllers")
 
 router.get("/surveyHeaderId/:surveyHeaderId/section", surveyController.GetSection);
-router.get("/userId/:userId/conutryId/:countryId/surveyHeaderId/:surveyHeaderId/surveySectionId/:surveySectionId", surveyController.getQuestion);
+router.get("/userId/:userId/countryId/:countryId/surveyHeaderId/:surveyHeaderId/surveySectionId/:surveySectionId", surveyController.getQuestion);
 
 module.exports=router
